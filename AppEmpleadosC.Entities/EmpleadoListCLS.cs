f7d6a300b48850c33b3cebdf73925699b72fc308
Aldirapc <aldirapigui2005@gmail.com>
2025-08-26 14:44:36 -0400

    ACTIVIDAD 2

---
+++ AppEmpleadosC.Entities/EmpleadoListCLS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEmpleadosC.Entities
+{
+    public class EmpleadoListCLS
+    {
+        public int Num_Empl { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Cargo { get; set; } = null!;
+    }
+}
