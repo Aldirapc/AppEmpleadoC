dc8215729a3f3cefdf06883a2d4becec5091176b
Aldirapc <aldirapigui2005@gmail.com>
2025-09-23 14:26:15 -0400

    AVANCE TAREA

---
+++ AppEmpleadosC.Entities/ClienteFormCLS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace AppEmpleadosC.Entities
+{
+    public class ClienteFormCLS
+    {
+        [Required(ErrorMessage = "El número de cliente es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de cliente debe ser positivo")]
+        public int CodigoCliente { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres")]
+        public string NombreCliente { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debe tener un Representante de Ventas asignado")]
+        [MaxLength(50, ErrorMessage = "El cargo no puede superar los 50 caracteres")]
+        public string Representante { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El limite de credito es obligatotio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El limite de redito es obligatorio")]
+        public decimal LimiteCredito { get; set; }
+
+ 
+    }
+}
